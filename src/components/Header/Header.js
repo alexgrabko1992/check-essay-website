7ce711221a1626a9dde2cb2d7b4aceb19348cf9c
stella <stelluchchka@gmail.com>
2025-02-16 14:56:23 +0300

    change logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import '../../App.css';
 import './Header.css'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const location = useLocation();
+  const currentUrl = location.pathname;
   return (
     <header className="header">
       <div className="header-left">
-        <h1>check essay</h1>
-        <p>сервис по подготовке к сочинению</p>
+      <div class="logo">
+        <Link to="/" className="logo-link"><span class="part1">eS</span><span class="part2">Say</span></Link>
+      </div>
       </div>
       <div className="header-right">
       {!isLoggedIn ? (
           <>
-            <button className="grey-btn">доступно проверок: 2</button>
-            <Link to="/essays"><button className="btn">сочинения</button></Link>
-            <Link to="/"><button className="btn">главная</button></Link>
-            <button className="btn">профиль</button>
-            {/* <Link to="/essays" className="link">сочинения</Link>
-            <Link to="/" className="link">главная</Link>
-            <Link to="/" className="link">профиль</Link> */}
+          <div className="nav-container">
+            <div className="header-right-content">
+              {/* 
+              <Link to="/"><button className="btn">главная</button></Link>
+              <Link to="/essays"><button className="btn">сочинения</button></Link>
+              <button className="btn">профиль</button> */}
+              <Link to="/" className={currentUrl==="/" ? "active-link": "link"}>главная</Link>
+              <Link to="/variants" className={currentUrl==="/variants" ? "active-link": "link"}>варианты</Link>
+              <Link to="/essays" className={currentUrl==="/essays" ? "active-link": "link"}>сочинения</Link>
+              <Link to="/profile" className={currentUrl==="/profile" ? "active-link": "link"}>профиль</Link>
+            </div>
+              <div className="checks">доступно проверок: 2</div>
+            </div>
+          
           </>
         ) : (
           <>
