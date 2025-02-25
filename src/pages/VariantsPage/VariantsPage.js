4740292deda82d820e83dcc4701250233aab6fd8
stella <stelluchchka@gmail.com>
2025-02-26 00:39:11 +0300

    remove Variant Grid

--- src/pages/VariantsPage/VariantsPage.js
+++ src/pages/VariantsPage/VariantsPage.js
@@ -1,14 +1,37 @@
 import Header from '../../components/Header/Header';
-import VariantGrid from '../../components/VariantGrid/VariantGrid';
+import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import './VariantsPage.css';
 
 function VariantsPage() {
+  const navigate = useNavigate();
+  const [variantsCount, setVariantsCount] = useState(0);
+
+  useEffect(() => {
+        fetch('http://localhost:8080/counts')
+        .then(response => response.json())
+        .then(data => {
+            setVariantsCount(data["variants_count"]);
+        })
+        .catch(error => {
+            console.error('Error fetching variants count:', error);
+        });
+  }, []);
+
+  useEffect(() => {}, [variantsCount]);
   return (
-    <div className="App">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <div>
       <Header />
-      <main>
-        <VariantGrid />
-      </main>
+      <section className="variant-section">
+        <h2>База вариантов</h2>
+        <div className="variant-grid">
+          {[...Array(variantsCount)].map((_, i) => (
+            <button key={i} className="variant-btn" onClick={() => navigate(`/essay-input/${i + 1}`)}>вариант {i+1} 
+        </button>
+          ))}
+        </div>
+        <button className="btn-create" onClick={() => navigate(`/essay-input/0`)}>написать сочинение по своему варианту</button>
+      </section>
     </div>
   );
 }
